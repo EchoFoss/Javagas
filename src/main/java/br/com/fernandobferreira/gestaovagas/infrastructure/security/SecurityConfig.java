a0b994ae65d6d14f37301ee8017b34edc72de7f3
Fernando <fernando.balieiro@outlook.com>
2024-06-13 13:24:23 -0300

    base security config

--- src/main/java/br/com/fernandobferreira/gestaovagas/infrastructure/security/SecurityConfig.java
+++ src/main/java/br/com/fernandobferreira/gestaovagas/infrastructure/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.fernandobferreira.gestaovagas.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +11,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> {
+                auth
+                    .requestMatchers("/api/v1/candidate/").permitAll()
+                    .requestMatchers("/api/v1/company/").permitAll();
+
+                auth
+                    .anyRequest().authenticated();
+            });
         return http.build();
     }
 }
